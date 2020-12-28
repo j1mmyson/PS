fe8d11bb0341e3f276d6b749e0f89e415328066d
j1mmyson <bwson98@gmail.com>
2020-12-28 14:15:07 +0900

    백준 G5_1759_암호만들기

---
+++ python/baekjoon/GOLD/G5_1759_암호만들기.py
@@ -0,0 +1,14 @@
+import sys
+
+[l, c] = sys.stdin.readline().rstrip().split(' ')
+l, c = int(l), int(c)
+
+alp = sys.stdin.readline().rstrip().split(' ')
+
+answer = []
+alp = sorted(alp)
+temp = ''
+
+
+
+print(answer)
